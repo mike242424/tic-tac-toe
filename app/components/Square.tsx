11292a438f791a7cf3e5697f27258530809ff3af
mike242424 <mikebyers24@gmail.com>
2024-01-09 18:09:57 -0500

    react-tic-tac-toe

---
+++ app/components/Square.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+interface SquareProps {
+  value: string;
+  handleClick: () => void;
+}
+
+const Square = ({ value, handleClick }: SquareProps) => {
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
+};
+
+export default Square;
